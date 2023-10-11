0cfbb8a4a5d696f7d2f2c3a823f19721fe359639
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-11 10:58:00 +0530

    day2

--- src/components/Child.js
+++ src/components/Child.js
@@ -4,11 +4,14 @@ const Child = (props) => {
     console.log(props);
     // console.log(props.children);
 
+    const childData = 'Monu';
+
     const parentDataInChild = props.passDataToChild;
 
     return (
         <div>
             <h1>Child Component</h1>
+            <p>Child data in child: {childData}</p>
             <p>Parent data in child: {parentDataInChild}</p>
         </div>
     );
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -4,11 +4,13 @@ import Child from './Child';
 const Parent = () => {
 
     const parentData = 'Sonu';
+    const [childDataInParent, setChildDataInParent] = useState('');
 
     return (
         <div>
             <h1>Parent Component</h1>
             <p>Parent data in parent: {parentData}</p>
+            <p>Child data in parent: {childDataInParent}</p>
             {/* <Child /> */}
             {/* <Child>{parentData}</Child> */}
             <Child passDataToChild={parentData} />
