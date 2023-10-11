a7022235572620b2f71877453a5a8cf3b15d71fc
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-11 10:04:31 +0530

    day2

--- src/components/DemoForm.js
+++ src/components/DemoForm.js
@@ -1,29 +1,42 @@
 import { useState } from "react";
-
 const DemoForm = () => {
-
     const [demoData, setDemoData] = useState({
         username: '',
         password: ''
     });
-
+    const handleUserData = (evt) => {
+        console.log(evt.target.value);
+        console.log(evt.target.value);
+        setDemoData({
+            ...demoData,
+            [evt.target.name]: evt.target.value
+        });
+    };
+    const submitUserData = (evt) => {
+        console.log(evt);
+        alert(`Hi ${demoData.username}!`);
+        evt.preventDefault();
+        setDemoData({username: '', password: ''});
+    };
     return (
         <div>
             <h1>Demo Form</h1>
             <p>Form is used to capture user inputs.</p>
             <div>
-                <form>
-                    <input type='text' value='' />
+                <form onSubmit={submitUserData}>
+                    <input type="text" name="username" 
+                    value={demoData.username} onChange={handleUserData} />
                     <br />
-                    <input type='password' value='' />
+                    <input type="password" name="password" 
+                    value={demoData.password} onChange={handleUserData} />
                     <br />
-                    <input type='submit' value='Submit' />
+                    <input type="submit" value="Submit" />
                 </form>
             </div>
             <div>
                 <p>Data as entered</p>
-                <p>{demoData.username}</p>
-                <p>{demoData.password}</p>
+                <p>Username: {demoData.username}</p>
+                <p>Password: {demoData.password}</p>
             </div>
         </div>
     );
