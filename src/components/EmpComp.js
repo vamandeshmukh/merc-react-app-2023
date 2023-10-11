954a413b1cdd73a4296a081ef5cbebdc92cfc976
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-11 15:10:48 +0530

    day2

--- src/components/EmpComp.js
+++ src/components/EmpComp.js
@@ -2,28 +2,79 @@ import { useEffect, useState } from "react";
 
 const EmpComp = () => {
 
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
     // fetch 
     // axios 
 
     const [emp, setEmp] = useState({});
+    const [eidToSearch, setEidToSearch] = useState('');
 
     useEffect(() => { });
 
-    const findEmployeeById = () => {
-        // fetch
-     };
+    const handleEmployeeId = (evt) => {
+        setEidToSearch(evt.target.value);
+    };
+
+    const findEmployeeById = (evt) => {
+
+        fetch(`${apiUrl}/${eidToSearch}`)
+            .then((res) => { return res.json(); })
+            .then((resp) => {
+                console.log(resp);
+                setEmp(resp);
+                setEidToSearch('');
+            })
+            .catch((err) => { console.log(err); });
+        evt.preventDefault();
+    };
 
     return (
         <div>
             <h1>Employee Component</h1>
             <div>
                 <p>Find Employee by Id</p>
-                <input type="number" name="id" placeholder="Employee id" required />
-                <input type="button" onClick={findEmployeeById} value="Submit" />
+                <form onSubmit={findEmployeeById}>
+                    <input type="number" name="eidToSearch" value={eidToSearch} onChange={handleEmployeeId} placeholder="Employee id" required />
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
             <div>
                 <p>Employee Data</p>
-                <p>Name: </p>
+                <div>
+                    {emp &&
+                        <div>
+                            <p>Name: {emp.name}</p>
+                            <p>Username: {emp.username}</p>
+                            <div>
+                                {emp.address &&
+                                    <div>
+                                        <p>Address:</p>
+                                        <p>Street: {emp.address.street}</p>
+                                        <p>Suite: {emp.address.suite}</p>
+                                        <p>City: {emp.address.city}</p>
+                                        <p>Zipcode: {emp.address.zipcode}</p>
+                                        {/* <div>
+                                            {emp.address.geo &&
+                                                <div>
+                                                    <p>Location:</p>
+                                                    <p>Lattitude: {emp.address.geo.lat}</p>
+                                                    <p>Longitude: {emp.address.get.lng}</p>
+                                                </div>
+                                            }
+                                        </div> */}
+                                    </div>
+                                }
+                            </div>
+                            <p>Email: {emp.phone}</p>
+                            <p>Website: {emp.website}</p>
+                            <p>Company:</p>
+                            {/* <p>Name: {emp.company.name}</p> */}
+                            {/* <p>Catchphrase: {emp.company.catchPhrase}</p> */}
+                            {/* <p>BS: {emp.company.bs}</p> */}
+                        </div>
+                    }
+                </div>
             </div>
         </div>
     );
