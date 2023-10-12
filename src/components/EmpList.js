07154ce774dce2dfc07805c423c46cf9d013086e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-12 10:34:01 +0530

    day-3

--- src/components/EmpList.js
+++ src/components/EmpList.js
@@ -4,22 +4,26 @@ import { getAllEmps } from "../services/EmpService";
 const EmpList = () => {
 
     const [allEmpList, setAllEmpList] = useState([]);
-    
+    const [errMessage, setErrMessage] = useState('');
+
     useEffect(() => {
         getAllEmps()
             .then((resp) => {
                 console.log(resp.data);
                 setAllEmpList(resp.data); // use response with 'data' 
+                setErrMessage('');
             })
             .catch((err) => {
-                // write proper code 
                 console.log(err);
+                setErrMessage(`${err.message}`);
+                setAllEmpList([]);
             })
     }, []);
 
     return (
         <div>
             <h1>List of All Employees</h1>
+            <p>{errMessage}</p>
             {
                 allEmpList.map((emp, k) => {
                     return <p obj={emp} key={k}> {emp.name} </p>
