8b7f98199627b03b3295726d44e17b135a322069
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-10-13 09:04:15 +0530

    day 4

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -5,3 +5,20 @@
 // 4. Create reducers / slices 
 // 5. Send data to the store 
 // 6. Use the data wherever needed 
+
+import { configureStore } from "@reduxjs/toolkit";
+
+// const store = {};
+// const store = configureStore();
+// const store = configureStore({});
+// const store = configureStore({
+//     reducer: {}
+// });
+
+const store = configureStore({
+    reducer: {
+        // reducers to be created 
+    }
+});
+
+export default store;
